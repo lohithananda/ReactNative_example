9afed30df72593562cfe2e1cb8b78e342469d40e
lohith <lohith@codewave.in>
2017-06-15 16:16:19 +0530

    'ChangedLogin'

--- app/components/LoginCom/Login.js
+++ app/components/LoginCom/Login.js
@@ -10,27 +10,14 @@ import {
 } from 'react-navigation';
 import {LoginCon} from './LoginContainer';
 
-const Realm = require('realm');
-
-
 export default class Login extends React.Component{
     static navigationOptions = {
         title:'lohith',
     }
     render(){
-         let realm = new Realm({
-             schema: [{name: 'Dog', properties: {name: 'string'}}]
-            });
-            realm.write(() => {
-                realm.create('Dog', {name: 'Rex'});
-            });
         const { navigate } = this.props.navigation;
         return(
-            
             <View>
-                {/*<Text>
-                     Count of Dogs in Realm: {realm.objects('Dog').length}
-                </Text>*/}
                <LoginCon {...this.props.navigation}/>
             </View>
         )
